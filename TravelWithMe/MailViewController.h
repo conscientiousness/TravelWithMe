1aa8f1cabb9598065d0d5b2bac4aaebe3a9638b0
HankHsu1010 <hankhsu1010@gmail.com>
2015-07-26 08:57:00 +0800

    修改setting

---
+++ TravelWithMe/MailViewController.h
@@ -0,0 +1,13 @@
+//
+//  MailViewController.h
+//  TravelWithMe
+//
+//  Created by Hank on 2015/7/25.
+//  Copyright (c) 2015年 Jesse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MailViewController : UIViewController
+
+@end
