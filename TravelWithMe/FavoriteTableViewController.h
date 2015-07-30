bc5c8af09a17d5cbdc9133ddb573b23251baf243
Jesse Lin <n823n823@gmail.com>
2015-07-30 20:29:07 +0800

    update tableviewcontroller

--- TravelWithMe/FavoriteTableViewController.h
+++ TravelWithMe/FavoriteTableViewController.h
@@ -2,7 +2,7 @@
 //  FavoriteTableViewController.h
 //  TravelWithMe
 //
-//  Created by ajay on 2015/7/29.
+//  Created by Jesselin on 2015/7/30.
 //  Copyright (c) 2015年 Jesse. All rights reserved.
 //
 
