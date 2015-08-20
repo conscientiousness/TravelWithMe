617b019f3fa6edea5e61f1f69e91a5fdb457baba
HankHsu1010 <hankhsu1010@gmail.com>
2015-08-20 22:25:06 +0800

    1.新增隱私權欄位 2.註解幫我評分

---
+++ TravelWithMe/PolicyViewController.h
@@ -0,0 +1,13 @@
+//
+//  PolicyViewController.h
+//  TravelWithMe
+//
+//  Created by Hank on 2015/8/20.
+//  Copyright (c) 2015年 Jesse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PolicyViewController : UIViewController
+
+@end
---
+++ TravelWithMe/PolicyViewController.m
@@ -0,0 +1,67 @@
+//
+//  PolicyViewController.m
+//  TravelWithMe
+//
+//  Created by Hank on 2015/8/20.
+//  Copyright (c) 2015年 Jesse. All rights reserved.
+//
+
+#import "PolicyViewController.h"
+
+@interface PolicyViewController ()
+{
+    NSArray *policytextviewAry;
+}
+@property (weak, nonatomic) IBOutlet UITextView *policyTextview;
+
+@end
+
+@implementation PolicyViewController
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    MBProgressHUD *hud =  [MBProgressHUD showHUDAddedTo:self.view animated:YES];
+    hud.labelText = @"讀取中...";
+    
+    dispatch_queue_t loadingQueue = dispatch_queue_create("loading", nil);
+    dispatch_async(loadingQueue, ^{
+        [self getdata];
+        dispatch_async(dispatch_get_main_queue(), ^{
+            _policyTextview.text = policytextviewAry[0][@"text"];
+            [MBProgressHUD hideHUDForView:self.view animated:YES];
+        });
+    });
+    
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Load Parse Data
+
+- (void) getdata{
+    PFQuery *policytext = [PFQuery queryWithClassName:@"Policy"];
+    
+    policytextviewAry = [[NSArray alloc] initWithArray:[policytext findObjects]];
+    
+}
+
+
+/*
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
+
+@end
