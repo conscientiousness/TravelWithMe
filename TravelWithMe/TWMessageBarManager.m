86e7d6e34f4ff7b6476ebeb65e6a1dc662773952
HankHsu1010 <hankhsu1010@gmail.com>
2015-08-28 15:07:16 +0800

    新增網路判斷 Alert

--- TravelWithMe/TWMessageBarManager.m
+++ TravelWithMe/TWMessageBarManager.m
@@ -756,7 +756,7 @@ static UIColor *kTWDefaultMessageBarStyleSheetInfoStrokeColor = nil;
         // Colors (background)
         kTWDefaultMessageBarStyleSheetErrorBackgroundColor = [UIColor colorWithRed:1.0 green:0.611 blue:0.0 alpha:kTWMessageBarStyleSheetMessageBarAlpha]; // orange
         kTWDefaultMessageBarStyleSheetSuccessBackgroundColor = [UIColor colorWithRed:0.0f green:0.831f blue:0.176f alpha:kTWMessageBarStyleSheetMessageBarAlpha]; // green
-        kTWDefaultMessageBarStyleSheetInfoBackgroundColor = [UIColor colorWithRed:0.0 green:0.482 blue:1.0 alpha:kTWMessageBarStyleSheetMessageBarAlpha]; // blue
+        kTWDefaultMessageBarStyleSheetInfoBackgroundColor = [UIColor colorWithRed:1.0 green:0.5 blue:0.0 alpha:kTWMessageBarStyleSheetMessageBarAlpha]; // blue
         
         // Colors (stroke)
         kTWDefaultMessageBarStyleSheetErrorStrokeColor = [UIColor colorWithRed:0.949f green:0.580f blue:0.0f alpha:1.0f]; // orange
